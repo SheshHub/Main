2dd5c665e54c8e5b4858c085e284eb91296b16bf
SheshHub <89948229+SheshHub@users.noreply.github.com>
2021-09-02 01:44:14 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,4 @@
 print("Loading...")
+if game.PlaceId == 6766156863 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Strongman%20Simulator.lua", true))()
+end
