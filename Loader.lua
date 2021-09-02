8d4cf1120fd10bba0193bfc93da8027f4a881041
SheshHub <89948229+SheshHub@users.noreply.github.com>
2021-09-02 03:58:41 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,3 +5,6 @@ end
 if game.PlaceId == 6284583030 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Pet%20Simulator%20X.lua", true))()
 end
+if game.PlaceId == 2512643572 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Bubblegum%20Simulator.lua", true))()
+end
