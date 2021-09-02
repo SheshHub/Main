32d74831bc4727fc206574361eb02bb724ac1897
SheshHub <89948229+SheshHub@users.noreply.github.com>
2021-09-02 02:43:35 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,3 +2,6 @@ print("Loading...")
 if game.PlaceId == 6766156863 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Strongman%20Simulator.lua", true))()
 end
+if game.PlaceId == 6284583030 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Pet%20Simulator%20X.lua", true))()
+end
