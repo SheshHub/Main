89c4e4bfc5bfc746c04c8133acc1b5d4b787654d
SheshHub <89948229+SheshHub@users.noreply.github.com>
2021-09-12 14:18:16 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,19 @@
-print("Loading...")
-if game.PlaceId == 6766156863 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Strongman%20Simulator.lua", true))()
-end
-if game.PlaceId == 6284583030 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Pet%20Simulator%20X.lua", true))()
-end
-if game.PlaceId == 2512643572 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Bubblegum%20Simulator.lua", true))()
-end
-if game.PlaceId == 7321682575 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Farm%20Yard%20Simulator.lua", true))()
-end
-if game.PlaceId == 4058282580 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SheshHub/Main/main/games/Boxing%20Simulator.lua", true))()
+local MarketplaceService = game:GetService("MarketplaceService")
+
+local games = {
+    [6766156863] = {src="https://raw.githubusercontent.com/SheshHub/Main/main/games/Strongman%20Simulator.lua"},
+    [6284583030] = {src="https://raw.githubusercontent.com/SheshHub/Main/main/games/Pet%20Simulator%20X.lua"},
+    [2512643572] = {src="https://raw.githubusercontent.com/SheshHub/Main/main/games/Bubblegum%20Simulator.lua"},
+    [7321682575] = {src="https://raw.githubusercontent.com/SheshHub/Main/main/games/Farm%20Yard%20Simulator.lua"},
+    [4058282580] = {src="https://raw.githubusercontent.com/SheshHub/Main/main/games/Boxing%20Simulator.lua"},
+  
+} 
+
+local check = games[game.PlaceId] 
+
+if check then
+    loadstring(game:HttpGet(check.src, true))()
+else
+    local Players = game:GetService("Players")
+    Players.LocalPlayer:Kick(string.format("%s is unsupported.", MarketplaceService:GetProductInfo(game.PlaceId).Name))
 end
